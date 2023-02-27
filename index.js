d8197e1fab5a83f63c89d6fd1b01e2bdb09b76b9
Виталий Анофриев <deyn1995@mail.ru>
2023-02-27 15:42:41 +0300

    Ищу ошибку

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+  
+
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
